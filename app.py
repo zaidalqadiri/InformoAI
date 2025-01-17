ae929393ab56912f7931b4f9f4c809ddfdedfedf
Zaid <zaidalqadiri@gmail.com>
2025-01-16 20:37:42 -0700

    Created text_input for user questions and generated a response based on the inputted documents and openAI

--- app.py
+++ app.py
@@ -6,13 +6,16 @@ from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
+from langchain_openai import OpenAI
+from langchain.chains.question_answering import load_qa_chain
 
 def main():
     st.header("InformoAI💬")
 
-    # load_dotenv()
+    load_dotenv()
 
     pdf = st.file_uploader("Upload your pdf", type='pdf')
+    vectorStore = None
 
     if pdf is not None:
         pdf_reader = PdfReader(pdf)
@@ -44,7 +47,21 @@ def main():
 
             # Save the FAISS vector store
             vectorStore.save_local(store_name)
-            st.success(f"Embeddings stored successfully")
+            st.success(f"Embeddings saved successfully")
+
+    # user question
+    query = st.text_input("Ask questions about your file:")
+    st.write(query)
+
+    if query:
+        # Semantic search to find chunks most similar to the query
+        docs = vectorStore.similarity_search(query=query, k=3)
+        
+        llm = OpenAI()
+        chain = load_qa_chain(llm=llm, chain_type='stuff')
+        response = chain.run(input_documents=docs, question=query)
+        st.write(response)
+
 
 if __name__ == '__main__':
     main()
